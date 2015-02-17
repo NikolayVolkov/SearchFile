879c9baf427041b0392d755d236c17e354e35775
NikolayVolkov <omkoluser@gmail.com>
2015-02-17 17:36:00 +0300

    New project created
    SearchFiles

---
+++ SearchFiles/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace SearchFiles
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.selectDirButton = new System.Windows.Forms.Button();
+            this.startDirTextBox = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.textBoxMask = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.infoFileLabel = new System.Windows.Forms.Label();
+            this.processFileLabel = new System.Windows.Forms.Label();
+            this.infoTimeLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textInFilesTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.treeView1.Location = new System.Drawing.Point(12, 193);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(418, 226);
+            this.treeView1.TabIndex = 0;
+            // 
+            // selectDirButton
+            // 
+            this.selectDirButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.selectDirButton.Location = new System.Drawing.Point(332, 5);
+            this.selectDirButton.Name = "selectDirButton";
+            this.selectDirButton.Size = new System.Drawing.Size(99, 23);
+            this.selectDirButton.TabIndex = 1;
+            this.selectDirButton.Text = "Выбрать папку";
+            this.selectDirButton.UseVisualStyleBackColor = true;
+            this.selectDirButton.Click += new System.EventHandler(this.selectDirButton_Click);
+            // 
+            // startDirTextBox
+            // 
+            this.startDirTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.startDirTextBox.Location = new System.Drawing.Point(12, 29);
+            this.startDirTextBox.Name = "startDirTextBox";
+            this.startDirTextBox.Size = new System.Drawing.Size(418, 20);
+            this.startDirTextBox.TabIndex = 2;
+            // 
+            // textBoxMask
+            // 
+            this.textBoxMask.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxMask.Location = new System.Drawing.Point(12, 68);
+            this.textBoxMask.Name = "textBoxMask";
+            this.textBoxMask.Size = new System.Drawing.Size(418, 20);
+            this.textBoxMask.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Шаблон имени файла:";
+            // 
+            // startButton
+            // 
+            this.startButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.startButton.Location = new System.Drawing.Point(12, 164);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(418, 23);
+            this.startButton.TabIndex = 5;
+            this.startButton.Text = "Начать поиск";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // infoFileLabel
+            // 
+            this.infoFileLabel.AutoSize = true;
+            this.infoFileLabel.Location = new System.Drawing.Point(101, 130);
+            this.infoFileLabel.Name = "infoFileLabel";
+            this.infoFileLabel.Size = new System.Drawing.Size(216, 13);
+            this.infoFileLabel.TabIndex = 6;
+            this.infoFileLabel.Text = "Обработано 0 файлов, найдено 0 файлов";
+            // 
+            // processFileLabel
+            // 
+            this.processFileLabel.AutoSize = true;
+            this.processFileLabel.Location = new System.Drawing.Point(9, 148);
+            this.processFileLabel.Name = "processFileLabel";
+            this.processFileLabel.Size = new System.Drawing.Size(125, 13);
+            this.processFileLabel.TabIndex = 7;
+            this.processFileLabel.Text = "Обрабатывается файл:";
+            // 
+            // infoTimeLabel
+            // 
+            this.infoTimeLabel.AutoSize = true;
+            this.infoTimeLabel.Location = new System.Drawing.Point(9, 130);
+            this.infoTimeLabel.Name = "infoTimeLabel";
+            this.infoTimeLabel.Size = new System.Drawing.Size(92, 13);
+            this.infoTimeLabel.TabIndex = 8;
+            this.infoTimeLabel.Text = "Прошло 00:00:00";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(167, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Текст, содержащийся в файле:";
+            // 
+            // textInFilesTextBox
+            // 
+            this.textInFilesTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textInFilesTextBox.Location = new System.Drawing.Point(12, 107);
+            this.textInFilesTextBox.Name = "textInFilesTextBox";
+            this.textInFilesTextBox.Size = new System.Drawing.Size(418, 20);
+            this.textInFilesTextBox.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 10);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Стартовая директория:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(441, 431);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textInFilesTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.infoTimeLabel);
+            this.Controls.Add(this.processFileLabel);
+            this.Controls.Add(this.infoFileLabel);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxMask);
+            this.Controls.Add(this.startDirTextBox);
+            this.Controls.Add(this.selectDirButton);
+            this.Controls.Add(this.treeView1);
+            this.MinimumSize = new System.Drawing.Size(300, 400);
+            this.Name = "Form1";
+            this.Text = "Поиск файлов";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Button selectDirButton;
+        private System.Windows.Forms.TextBox startDirTextBox;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox textBoxMask;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label infoFileLabel;
+        private System.Windows.Forms.Label processFileLabel;
+        private System.Windows.Forms.Label infoTimeLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textInFilesTextBox;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ SearchFiles/Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using UITimer = System.Windows.Forms.Timer;
+
+namespace SearchFiles
+{
+    public partial class Form1 : Form
+    {
+        
+        private static bool run;
+        private static bool run2;
+        private UITimer timer = null;
+        private DateTime beginSearchTime;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        void GetFolders(DirectoryInfo d, TreeNode node)
+        {
+            try
+            {
+                DirectoryInfo[] dInfo = d.GetDirectories();
+                if (dInfo.Length > 0)
+                {
+                    TreeNode treeNode = new TreeNode();
+                    foreach (DirectoryInfo driSub in dInfo)
+                    {
+                        treeNode = node.Nodes.Add(driSub.Name);
+                        GetFolders(driSub, treeNode);
+                    }
+                }
+            }
+            catch { }
+        }
+
+        void GetFiles(DirectoryInfo d, TreeNode node)
+        {
+            FileInfo[] subfileInfo = d.GetFiles("*.*");
+            if (subfileInfo.Length > 0)
+            {
+                for (int j = 0; j < subfileInfo.Length; j++)
+                {
+                    node.Nodes.Add(subfileInfo[j].Name);
+                }
+            }
+        }
+        private void ListDirectory(string path, string mask, string findtext)
+        {
+            treeView1.Invoke((MethodInvoker)(() => treeView1.Nodes.Clear()));
+            int filesFound = 0;
+            int filesDone = 0;
+
+            var stack = new Stack<TreeNode>();
+            var rootDirectory = new DirectoryInfo(path);
+            var node = new TreeNode(rootDirectory.Name) { Tag = rootDirectory };
+            stack.Push(node);
+
+            while (stack.Count > 0 && run)
+            {
+                var currentNode = stack.Pop();
+                var directoryInfo = (DirectoryInfo)currentNode.Tag;
+                bool check = false;
+                try
+                {
+                    directoryInfo.GetDirectories();
+                    check = true;
+                }
+                catch { }
+                if (check) 
+                {
+                    foreach (var directory in directoryInfo.GetDirectories())
+                    {
+                        var childDirectoryNode = new TreeNode(directory.Name) { Tag = directory };
+                        currentNode.Nodes.Add(childDirectoryNode);
+                        stack.Push(childDirectoryNode);
+                        if (!run) break;
+                    }
+                    foreach (var file in directoryInfo.GetFiles(mask))
+                    {
+                        processFileLabel.Invoke((MethodInvoker)delegate
+                        { processFileLabel.Text = "Обрабатывается файл: " + file.FullName; });
+                        System.IO.StreamReader rdfile = new System.IO.StreamReader(file.FullName);
+                        string line;
+                        while ((line = rdfile.ReadLine()) != null)
+                        {
+                            if (line.Contains(findtext)) //Found file with specified text
+                            {
+                                string[] split = file.FullName.Split('\\'); //Split path into dir names and file name
+                                string splitname = "";
+                                TreeNode savedresult = null; bool findlastresult = false;
+                                foreach (string splitpart in split) //Code for realtime refreshing treeview and don't adding empty folders
+                                {
+                                    splitname += splitpart + "\\";
+                                    TreeNode[] result;
+                                    if (findlastresult)
+                                    { result = savedresult.Nodes.Find(splitname, true); }
+                                    else
+                                    { result = treeView1.Nodes.Find(splitname, true); }
+                                    if (result.Length == 0)
+                                    {
+                                        if (findlastresult)
+                                        { treeView1.Invoke((MethodInvoker)(() => savedresult.Nodes.Add(splitname, splitpart))); }
+                                        else
+                                        { treeView1.Invoke((MethodInvoker)(() => treeView1.Nodes.Add(splitname, splitpart))); }
+                                        result = treeView1.Nodes.Find(splitname, true);   
+                                    }
+                                    savedresult = result[0];
+                                    findlastresult = true;
+                                }
+                                currentNode.Nodes.Add(new TreeNode(file.Name));
+                                filesFound++;
+                                
+                                break;
+                            }
+                            if (!run) break;
+                        }
+
+                        filesDone++;
+                        if (!run) break;
+                        infoFileLabel.Invoke((MethodInvoker)delegate
+                        { infoFileLabel.Text = "Обработано " + filesDone.ToString() + " файлов, найдено " + filesFound.ToString() + " файлов"; });
+                    }
+                }
+            }
+
+            setStopSearchState();
+        }
+
+        private void selectDirButton_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                startDirTextBox.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (run2)
+            {
+                TimeSpan span = DateTime.Now.Subtract(beginSearchTime);
+                infoTimeLabel.Text = "Прошло " + new DateTime(span.Ticks).ToString("HH:mm:ss tt");
+            }
+            else this.timer.Enabled = false;
+        }
+        private void setStopSearchState()
+        {
+            run = false;
+            run2 = false;
+            startButton.BeginInvoke(new Action(() => { startButton.Text = "Начать поиск"; }));
+        }
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            if (run)
+            {
+                setStopSearchState();
+            }
+            else
+            {
+                if (textBoxMask.Text == "")
+                {
+                    MessageBox.Show("Не задан шаблон имени файла.");
+                }
+                else
+                {
+                    string path = startDirTextBox.Text;
+                    if (System.IO.Directory.Exists(path))
+                    {
+                        run = true; run2 = true;
+                        startButton.Text = "Остановить поиск";
+                        infoTimeLabel.Text = "Прошло 00:00:00";
+                        infoFileLabel.Text = "Обработано 0 файлов, найдено 0 файлов";
+                        beginSearchTime = DateTime.Now;
+                        this.timer = new UITimer();
+                        this.timer.Interval = 500;
+                        this.timer.Tick += new EventHandler(timer_Tick);
+                        this.timer.Enabled = true;
+                        Thread searchThread = new Thread(delegate() { ListDirectory(path, textBoxMask.Text, textInFilesTextBox.Text); });
+                        searchThread.Start();
+                        searchThread.IsBackground = true;
+                    }
+                    else MessageBox.Show("Неправильная стартовая директория.");
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            startDirTextBox.Text = Properties.Settings.Default.startDir;
+            textBoxMask.Text = Properties.Settings.Default.mask;
+            textInFilesTextBox.Text = Properties.Settings.Default.textInFiles;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Properties.Settings.Default["startDir"] = startDirTextBox.Text;
+            Properties.Settings.Default["mask"] = textBoxMask.Text;
+            Properties.Settings.Default["textInFiles"] = textInFilesTextBox.Text;
+            Properties.Settings.Default.Save();
+        }
+
+    }
+}
